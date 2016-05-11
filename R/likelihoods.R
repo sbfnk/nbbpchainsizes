1b44351b3d051bcb85ab65717b7b07bca2f431b3
Sebastian Funk <sbfnk@users.noreply.github.com>
2016-05-11 07:51:42 +0100

    introduce infinity

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -7,15 +7,40 @@
 ##' @param log hether to return log-likelihood
 ##' @return likelihood
 ##' @author Sebastian Funk
-nbbpLikelihood <- function(chains, k, R, log = T) {
+nbbpLikelihood <- function(chains, k, R, log = TRUE, exclude.1 = FALSE, infinite = Inf) {
+    point.likelihood <- function(x, log = TRUE) {
+      if (log) {
+        ret <- -Inf
+        if (x >= infinite) {
+          ret <- log1p(-exp(logSumExp(sapply(seq_len(infinite - 1), function(x) {
+            lgamma(k * x + x - 1) - (lgamma(k * x) + lgamma(x + 1)) + (x - 1) * log (R / k) - (k * x + x - 1) * log(1 + R / k)
+          }))))
+        }
+        if (!is.finite(ret)) {
+          ret <- lgamma(k * x + x - 1) - (lgamma(k * x) + lgamma(x + 1)) + (x - 1) * log (R / k) - (k * x + x - 1) * log(1 + R / k)
+        }
+      } else {
+        if (x >= infinite) {
+          ret <- 1 - sum(sapply(seq_len(infinite - 1), function(x) {
+            gamma(k * x + x - 1) / (gamma(k * x) * gamma(x + 1)) * (R / k) ^ (x - 1) / (1 + R / k) ^ (k * x + x - 1)
+          }))
+        } else {
+          ret <- gamma(k * x + x - 1) / (gamma(k * x) * gamma(x + 1)) * (R / k) ^ (x - 1) / (1 + R / k) ^ (k * x + x - 1)
+        }
+      }
+      return(ret)
+    }
+    chain.likelihoods <- sapply(chains, point.likelihood)
     if (log) {
-      result <- sum(sapply(chains, function(x) {
-        lgamma(k * x + x - 1) - (lgamma(k * x) + lgamma(x + 1)) + (x - 1) * log (R / k) - (k * x + x - 1) * log(1 + R / k)
-      }))
+      result <- sum(chain.likelihoods)
+      if (exclude.1) {
+          result <- result - point.likelihood(1) 
+      }
     } else {
-      result <- prod(sapply(chains, function(x) {
-        gamma(k * x + x - 1) / gamma(k * x) * gamma(x + 1) * (R / k) ^ (x - 1) / (1 + R / k) ^ (k * x + x - 1)
-      }))
+      result <- prod(chain.likelihoods)
+      if (exclude.1) {
+          result <- result / point.likelihood(1) 
+      }
     }
     return(result)
 }
@@ -31,11 +56,54 @@ nbbpLikelihood <- function(chains, k, R, log = T) {
 ##' @param log hether to return log-likelihood
 ##' @return likelihood
 ##' @author Sebastian Funk
-nbbpPointSourceLikelihood <- function(chains, k, R, log = F) {
+nbbpPointSourceLikelihood <- function(chains, lik, log = TRUE, ...) {
   if (log) {
-    result <- nbbpLikelihood(chains, k, R, log) - nbbpLikelihood(rep(0, length(chains)), k, R, log)
+      result <- lik(chains, log = log, ...) -
+          lik(rep(0, length(chains)), log = log, ...)
   } else {
-    result <- nbbpLikelihood(chains, k, R, log) / nbbpLikelihood(rep(0, length(chains)), k, R, log)
+      result <- lik(chains, log = log, ...) /
+          lik(rep(0, length(chains)), log = log, ...)
   }
   return(result)
+}
+
+##' Likelihood for the outcome of a branching process with mixed
+##' gamma-borel outbreak distribution
+##'
+##' @param chains vector of sizes to which to match the branching process
+##' @param alpha alpha parameter of the gamma distribution
+##' @param beta beta parameter of the gamma distribution
+##' @param log hether to return log-likelihood
+##' @return likelihood
+##' @author Sebastian Funk
+gborLikelihood <- function(chains, k, R, log = T, exclude.1 = FALSE, infinite = Inf) {
+    point.likelihood <- function(x, log = TRUE) {
+      if (log) {
+        ret <- -Inf
+        if (x >= infinite) {
+          ret <- log1p(-exp(logSumExp(sapply(seq_len(infinite - 1), function(x) {
+            lgamma(k + x - 1) - (lgamma(x + 1) + lgamma(k)) - k * log(R / k) + (x-1) * log(x) - (k + x - 1) * log(x + k / R)
+          }))))
+        }
+        if (!is.finite(ret)) {
+          ret <- lgamma(k + x - 1) - (lgamma(x + 1) + lgamma(k)) - k * log(R / k) + (x-1) * log(x) - (k + x - 1) * log(x + k / R)
+        }
+      } else {
+        ret <- gamma(k + x - 1) / (gamma(x + 1) * gamma(k)) * x ^ (x - 1) / (((R / k) ^ k) * (x + k / R)^(k + x - 1))
+      }
+      return(ret)
+    }
+    chain.likelihoods <- sapply(chains, point.likelihood)
+    if (log) {
+      result <- sum(chain.likelihoods)
+      if (exclude.1) {
+          result <- result - point.likelihood(1) 
+      }
+    } else {
+      result <- prod(chain.likelihoods)
+      if (exclude.1) {
+          result <- result / point.likelihood(1) 
+      }
+    }
+    return(result)
 }
--- R/simulations.R
+++ R/simulations.R
@@ -3,18 +3,50 @@
 ##'
 ##' @param R mean of the offspring distribution
 ##' @param k dispersion of the offspring distribution
-##' @param n number of outbreaks to simulato
+##' @param n number of outbreaks to simulate
 ##' @param init number of individuals in the first generation
 ##' @return vector of samples
 ##' @author Sebastian Funk
-nbbpChains <- function(R, k, n = 1, init = 1) {
+nbbpChains <- function(R, k, n = 1, init = 1, max) {
+    chains <- c()
+    for (i in 1:n) {
+    success <- FALSE
+    while (!success) {
+      newCases <- init
+      chainSize <- newCases
+      while (newCases > 0 & (missing(max) || (chainSize <= max))) {
+        newCases <- sum(rnbinom(newCases, k, 1 / (1 + R / k)))
+        chainSize <- chainSize + newCases
+      }
+      if (missing(max) || (chainSize <= max)) success <- TRUE
+    }
+    chains <- c(chains, chainSize)
+  }
+  chains
+}
+
+##' Generate chains from a branching process with mixed borel-gamma
+##' offspring distribution
+##'
+##' @param mean mean of the offspring distribution
+##' @param k dispersion of the offspring distribution
+##' @param n number of outbreaks to simulate
+##' @param init number of individuals in the first generation
+##' @return vector of samples
+##' @author Sebastian Funk
+gborChains <- function(R, k, n = 1, init = 1, max) {
   chains <- c()
   for (i in 1:n) {
-    newCases <- init
-    chainSize <- newCases
-    while (newCases > 0) {
-      newCases <- sum(rnbinom(newCases, k, 1 / (1 + R / k)))
-      chainSize <- chainSize + newCases
+    success <- FALSE
+    while (!success) {
+      newCases <- init
+      chainSize <- newCases
+      currentR <- rgamma(newCases, shape = R * k / (R + k), scale = 1 + R / k)
+      while (newCases > 0 & (missing(max) || (chainSize <= max))) {
+        newCases <- sum(rpois(newCases, currentR))
+        chainSize <- chainSize + newCases
+      }
+      if (missing(max) || (chainSize <= max)) success <- TRUE
     }
     chains <- c(chains, chainSize)
   }
