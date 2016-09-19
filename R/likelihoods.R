6ee03abfa7452a9f37d9b61098d3495379671c4f
sbfnk <sbfnk@users.noreply.github.com>
2016-09-19 09:09:45 +0100

    updated likelihoods

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -1,5 +1,13 @@
+  log.sum.exp<- function(x) {
+    ## Computes log(sum(exp(x))
+    ## Uses offset trick to avoid numeric overflow: https://stat.ethz.ch/pipermail/r-help/2011-February/269205.html
+    
+    xmax <- which.max(x)
+    log1p(sum(exp(x[-xmax]-x[xmax])))+x[xmax]
+  }
+
 ##' Likelihood for the outcome of a branching process with negative
-##' binomail offspring distribution
+##' binomial offspring distribution
 ##'
 ##' @param chains vector of sizes to which to match the branching process
 ##' @param k dispersion of the offspring distribution
@@ -7,42 +15,65 @@
 ##' @param log hether to return log-likelihood
 ##' @return likelihood
 ##' @author Sebastian Funk
-nbbpLikelihood <- function(chains, k, R, log = TRUE, exclude.1 = FALSE, infinite = Inf) {
-    point.likelihood <- function(x, log = TRUE) {
-      if (log) {
-        ret <- -Inf
-        if (x >= infinite) {
-          ret <- log1p(-exp(logSumExp(sapply(seq_len(infinite - 1), function(x) {
-            lgamma(k * x + x - 1) - (lgamma(k * x) + lgamma(x + 1)) + (x - 1) * log (R / k) - (k * x + x - 1) * log(1 + R / k)
-          }))))
-        }
-        if (!is.finite(ret)) {
-          ret <- lgamma(k * x + x - 1) - (lgamma(k * x) + lgamma(x + 1)) + (x - 1) * log (R / k) - (k * x + x - 1) * log(1 + R / k)
-        }
-      } else {
-        if (x >= infinite) {
-          ret <- 1 - sum(sapply(seq_len(infinite - 1), function(x) {
-            gamma(k * x + x - 1) / (gamma(k * x) * gamma(x + 1)) * (R / k) ^ (x - 1) / (1 + R / k) ^ (k * x + x - 1)
-          }))
-        } else {
-          ret <- gamma(k * x + x - 1) / (gamma(k * x) * gamma(x + 1)) * (R / k) ^ (x - 1) / (1 + R / k) ^ (k * x + x - 1)
-        }
-      }
-      return(ret)
-    }
-    chain.likelihoods <- sapply(chains, point.likelihood)
+nbbpLikelihood <- function(chains, k, R, log = TRUE, exclude.1 = FALSE, infinite = Inf)
+{
+  nb.likelihood <- function(x)
+  {
     if (log) {
-      result <- sum(chain.likelihoods)
-      if (exclude.1) {
-          result <- result - point.likelihood(1) 
-      }
+      lgamma(k * x + (x - 1)) - (lgamma(k * x) + lgamma(x + 1)) +
+        (x - 1) * log (R / k) -
+        (k * x + (x - 1)) * log(1 + R / k)
     } else {
-     result <- prod(chain.likelihoods)
-      if (exclude.1) {
-          result <- result / point.likelihood(1) 
-      }
+      gamma(k * x + (x - 1)) / (gamma(k * x) * gamma(x + 1)) * (R / k) ^ (x - 1) / (1 + R / k) ^ (k * x + (x - 1))
     }
-    return(result)
+  }
+
+  if (any(chains >= infinite)) {
+    calc_sizes <- seq_len(infinite)
+  } else {
+    calc_sizes <- unique(c(1, chains))
+  }
+
+  likelihoods <- c()
+  likelihoods[calc_sizes] <- sapply(calc_sizes, nb.likelihood)
+
+  if (exclude.1) {
+    if (log) {
+      likelihoods <- likelihoods - log(-expm1(likelihoods[1]))
+      likelihoods[1] <- -Inf
+    } else {
+      likelihoods <- likelihoods / (1 - likelihoods[1])
+      likelihoods[1] <- 0
+    }
+  }
+
+  if (log) {
+    sexpl <- sum(exp(likelihoods))
+    if (sexpl < 1) {
+      maxl <- log(1 - sum(exp(likelihoods)))
+    } else {
+      maxl <- -Inf
+    }
+  } else {
+    sl <- sum(likelihoods)
+    if (sl < 1) {
+      maxl <- 1 - sum(likelihoods)
+    } else {
+      maxl <- 0
+    }
+
+  }
+  likelihoods <- c(likelihoods, maxl)
+
+  chains[chains > infinite] <- infinite + 1
+  chain_likelihoods <- likelihoods[chains]
+
+  if (log) {
+    result <- sum(chain_likelihoods)
+  } else {
+    result <- prod(chain_likelihoods)
+  }
+  return(result)
 }
 
 ##' Likelihood for the outcome of a branching process with negative
@@ -67,8 +98,6 @@ nbbpPointSourceLikelihood <- function(chains, lik, log = TRUE, ...) {
   return(result)
 }
 
-<<<<<<< HEAD
-=======
 ##' Likelihood for the outcome of a branching process with mixed
 ##' gamma-borel outbreak distribution
 ##'
@@ -83,7 +112,7 @@ gborLikelihood <- function(chains, k, R, log = T, exclude.1 = FALSE, infinite = 
       if (log) {
         ret <- -Inf
         if (x >= infinite) {
-          ret <- log1p(-exp(logSumExp(sapply(seq_len(infinite - 1), function(x) {
+          ret <- log1p(-exp(log.sum.exp(sapply(seq_len(infinite - 1), function(x) {
             lgamma(k + x - 1) - (lgamma(x + 1) + lgamma(k)) - k * log(R / k) + (x-1) * log(x) - (k + x - 1) * log(x + k / R)
           }))))
         }
@@ -109,4 +138,3 @@ gborLikelihood <- function(chains, k, R, log = T, exclude.1 = FALSE, infinite = 
     }
     return(result)
 }
->>>>>>> origin/master
